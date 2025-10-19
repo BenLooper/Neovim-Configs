feb6b527149f1f225543523294f10917679e1cc9
BenLooper <benlooper08@gmail.com>
2025-10-19 11:07:29 -0500

    Added window navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,16 @@
+-- File navigation
 vim.keymap.set("n", "<Leader>e", "<cmd>Oil --preview --float<CR>", { desc="Open parent directory in Oil" });
+
+-- Clear search highlighting with normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Autocommand to highlight yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
