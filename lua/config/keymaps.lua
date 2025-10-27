ffd1a1f9a00418e2cca885c69d5981cf72958d59
BenLooper <benlooper08@gmail.com>
2025-10-27 07:30:37 -0500

    added conform, worked on dashboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,3 +23,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, 
   { desc = 'Open diagnostics in float' }
 )
+
+-- Formatting
+vim.keymap.set("n", "<leader>cf", function()
+    require("conform").format({
+        lsp_format = "fallback",
+    })
+end, { desc = "Format current file" })
