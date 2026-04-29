a2d864194f75fde6aff273bcc17053a3a85cfaf7
BLooper <benlooperdev@gmail.com>
2026-04-28 21:05:26 -0500

    added persistence, fixed dashboard actions, and added milli ascii gif

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,39 +1,87 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    local ascii_art = require('art')
-    require('dashboard').setup {
-      theme = 'doom',    -- theme is doom and hyper default is hyper
-      disable_move = true,      -- default is false disable move keymap for hyper
-      -- shortcut_type = 'letter',     -- shortcut type 'letter' or 'number'
-      config = {
-        header = ascii_art.hobbitHole,
-        center = {
-          {
-            icon = ' ',
-            icon_hl = 'Title',
-            desc = 'Find File           ',
-            desc_hl = 'String',
-            key = 'a',
-            keymap = 'SPC f f',
-            key_hl = 'Number',
-            key_format = ' %s', -- remove default surrounding `[]`
-            action = 'lua print(2)'
-          },
-          {
-            icon = ' ',
-            desc = 'Find Dotfiles',
-            key = 'f',
-            keymap = 'SPC f d',
-            key_format = ' %s', -- remove default surrounding `[]`
-            action = 'lua print(3)'
-          },
-        },
-        footer = {},
-        vertical_center = false, -- Center the Dashboard on the vertical (from top to bottom)
-      },
-    }
-  end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+	{
+		"amansingh-afk/milli.nvim",
+		lazy = false,
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			local splash = require("milli").load({ splash = "lights" })
+			require("dashboard").setup({
+				theme = "doom",
+				disable_move = true,
+				config = {
+					header = splash.frames[1],
+					center = {
+{
+						icon = "󰙅 ",
+						icon_hl = "Title",
+						desc = "File Explorer",
+						desc_hl = "String",
+						key = "e",
+						keymap = "SPC e",
+						key_hl = "Number",
+						key_format = " %s",
+						action = "Oil --preview --float",
+					},
+						{
+							icon = "󰋙 ",
+							icon_hl = "Title",
+							desc = "Recent Files",
+							desc_hl = "String",
+							key = "r",
+							keymap = "SPC f o",
+							key_hl = "Number",
+							key_format = " %s",
+							action = "lua require('fzf-lua').oldfiles()",
+						},
+						{
+							icon = "󰈱 ",
+							icon_hl = "Title",
+							desc = "Find Word",
+							desc_hl = "String",
+							key = "g",
+							keymap = "SPC f g",
+							key_hl = "Number",
+							key_format = " %s",
+							action = "lua require('fzf-lua').live_grep()",
+						},
+						{
+							icon = "󰒍 ",
+							icon_hl = "Title",
+							desc = "Session Picker",
+							desc_hl = "String",
+							key = "p",
+							keymap = "SPC f s",
+							key_hl = "Number",
+							key_format = " %s",
+							action = "PersistenceSelect",
+						},
+						{
+							icon = "󰦛 ",
+							icon_hl = "Title",
+							desc = "Restore Session",
+							desc_hl = "String",
+							key = "s",
+							keymap = "SPC q s",
+							key_hl = "Number",
+							key_format = " %s",
+							action = "PersistenceLoad",
+						},
+					},
+					footer = {},
+					vertical_center = false,
+				},
+			})
+			require("milli").dashboard({ splash = "lights", loop = true })
+			vim.api.nvim_create_user_command("PersistenceSelect", function()
+				require("persistence").select()
+			end, {})
+			vim.api.nvim_create_user_command("PersistenceLoad", function()
+				require("persistence").load()
+			end, {})
+		end,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 }
